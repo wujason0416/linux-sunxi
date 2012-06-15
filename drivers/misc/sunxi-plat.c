62b3dd1040cc0059ff948400bd081423efe7d0d7
Alejandro Mery <amery@geeks.cl>
2012-06-16 00:49:37 +0200

    misc: sunxi-plat: scan script.bin detecting sections that define features

--- drivers/misc/sunxi-plat.c
+++ drivers/misc/sunxi-plat.c
@@ -14,22 +14,98 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#define DEBUG
 #define pr_fmt(fmt)	"sunxi: " fmt
 
 #include <linux/module.h>
+
+#include <mach/memory.h>
+#include <plat/script.h>
+
+/*
+ * scan script.bin
+ */
+static int feature_name(const char *name, char *feature, int *id)
+{
+	char c;
+	int ret = 0;
+	size_t l = strlen(name);
+
+	if (l == 0)
+		goto done;
+
+	/* UARTs and USBs have the %d at the end */
+	c = name[l-1];
+	if (c >= '0' && c <= '9') {
+		*id = c - '0';
+		--l;
+	} else {
+		*id = -1;
+	}
+
+	if (l==4 && strncmp(name, "usbc", 4) == 0) {
+		/* USB controllers don't end in _para */
+		strcpy(feature, "usb");
+		ret = 1;
+	} else if (l<6) {
+		/* needs room for _para */
+		;
+	} else if (strncmp(name+l-5, "_para", 5) == 0) {
+		l -= 5;
+		if (*id < 0) {
+			c = name[l-1];
+			if (c >= '0' && c <= '9') {
+				*id = c - '0';
+				--l;
+				/* ps2_%d_para */
+				if (l>1 && name[l-1] == '_')
+					--l;
+			}
+		}
+		if (l > 0) {
+			memcpy(feature, name, l);
+			feature[l] = '\0';
+			ret = 1;
+		}
+	}
+done:
+	return ret;
+}
+
+static int config_scan(void)
+{
+	int i;
+	struct sunxi_script *head = (void*)__va(SYS_CONFIG_MEMBASE);
+	struct sunxi_script_section *section = head->section;
+
+	pr_debug("config: scanning %d sections at 0x%lx\n",
+		 head->count, SYS_CONFIG_MEMBASE);
+	for (i=head->count; i; i--, section++) {
+		char feature[32] = "";
+		int index = -1;
+
+		if (feature_name(section->name, feature, &index)) {
+			if (index < 0)
+				pr_debug("config: [%s] -> %s\n", section->name, feature);
+			else
+				pr_debug("config: [%s] -> %s:%d\n", section->name, feature, index);
+		} else {
+			pr_debug("config: [%s] SKIP\n", section->name);
+		}
+	}
+	return 0;
+}
 
 /*
  * module
  */
 static int sunxi_plat_init(void)
 {
-	pr_debug("init");
-	return 0;
+	return config_scan();
 }
 
 static void sunxi_plat_exit(void)
 {
-	pr_debug("exit");
 }
 
 module_init(sunxi_plat_init);
