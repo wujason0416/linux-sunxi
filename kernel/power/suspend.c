269358fcc31e6835bb98f8f7e256f74800b311a0
yanggq <yanggq@LServer2.(none)>
2012-09-13 14:07:02 +0800

    standby: modified kernel files

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -31,8 +31,12 @@
 #include "power.h"
 
 const char *const pm_states[PM_SUSPEND_MAX] = {
+#ifdef CONFIG_EARLYSUSPEND
+	[PM_SUSPEND_ON]		= "on",
+#endif
 	[PM_SUSPEND_STANDBY]	= "standby",
 	[PM_SUSPEND_MEM]	= "mem",
+	[PM_SUSPEND_BOOTFAST]	= "bootfast",
 };
 
 static const struct platform_suspend_ops *suspend_ops;
@@ -213,7 +217,7 @@ int suspend_devices_and_enter(suspend_state_t state)
 		if (error)
 			goto Close;
 	}
-	suspend_console();
+	//suspend_console();
 	ftrace_stop();
 	suspend_test_start();
 	error = dpm_suspend_start(PMSG_SUSPEND);
