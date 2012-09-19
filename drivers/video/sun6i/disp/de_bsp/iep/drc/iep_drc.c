c3d5cc03f56c21a3dd8ac80cd88f0b35be9e3805
tyle <tyle@LServer2.(none)>
2012-09-19 14:15:17 +0800

    fix cache bug, add clk & pin config

--- drivers/video/sun6i/disp/de_bsp/iep/drc/iep_drc.c
+++ drivers/video/sun6i/disp/de_bsp/iep/drc/iep_drc.c
@@ -31,9 +31,9 @@ __s32 drc_clk_init(__u32 sel)
 {
 	if(!sel)
 	{
-	    h_drcahbclk0 = OSAL_CCMU_OpenMclk(AW_MOD_CLK_AHB_DRC0);
-	    h_drcdramclk0 = OSAL_CCMU_OpenMclk(AW_MOD_CLK_SDRAM_DRC0);
-	    h_drcmclk0 = OSAL_CCMU_OpenMclk(AW_MOD_CLK_DRC0);
+	    h_drcahbclk0 = OSAL_CCMU_OpenMclk(AW_AHB_CLK_DRC0);
+	    h_drcdramclk0 = OSAL_CCMU_OpenMclk(AW_DRAM_CLK_DRC0);
+	    h_drcmclk0 = OSAL_CCMU_OpenMclk(AW_MOD_CLK_IEPDRC0);
 
 		OSAL_CCMU_MclkReset(h_drcmclk0, RST_INVAILD);
 		OSAL_CCMU_MclkOnOff(h_drcahbclk0, CLK_ON);
@@ -43,9 +43,9 @@ __s32 drc_clk_init(__u32 sel)
 	}
 	else
 	{
-		h_drcahbclk1 = OSAL_CCMU_OpenMclk(AW_MOD_CLK_AHB_DRC1);
-	    h_drcdramclk1 = OSAL_CCMU_OpenMclk(AW_MOD_CLK_SDRAM_DRC1);
-	    h_drcmclk1 = OSAL_CCMU_OpenMclk(AW_MOD_CLK_DRC1);
+		h_drcahbclk1 = OSAL_CCMU_OpenMclk(AW_AHB_CLK_DRC1);
+	    h_drcdramclk1 = OSAL_CCMU_OpenMclk(AW_DRAM_CLK_DRC1);
+	    h_drcmclk1 = OSAL_CCMU_OpenMclk(AW_MOD_CLK_IEPDRC1);
 
 		OSAL_CCMU_MclkReset(h_drcmclk1, RST_INVAILD);
 		OSAL_CCMU_MclkOnOff(h_drcahbclk1, CLK_ON);
