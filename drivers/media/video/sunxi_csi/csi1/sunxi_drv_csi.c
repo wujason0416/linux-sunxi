223e6411f302de17ce3fe95b98f2f7ec69110b3f
zenglingying <zenglingying@allwinnertech.com>
2013-01-08 10:51:17 +0800

    csi: fix bug for evb

--- drivers/media/video/sunxi_csi/csi1/sunxi_drv_csi.c
+++ drivers/media/video/sunxi_csi/csi1/sunxi_drv_csi.c
@@ -2654,6 +2654,10 @@ static int csi_release(void)
 			}
 		}
 
+#ifdef CONFIG_HAS_EARLYSUSPEND
+	unregister_early_suspend(&dev->early_suspend);
+#endif
+
 		v4l2_info(&dev->v4l2_dev, "unregistering %s\n", video_device_node_name(dev->vfd));
 		video_unregister_device(dev->vfd);
 		csi_clk_release(dev);
@@ -2671,10 +2675,7 @@ static int csi_release(void)
 
 static int __devexit csi_remove(struct platform_device *pdev)
 {
-#ifdef CONFIG_HAS_EARLYSUSPEND
-  struct csi_dev *dev=(struct csi_dev *)dev_get_drvdata(&(pdev)->dev);
-	unregister_early_suspend(&dev->early_suspend);
-#endif
+
 	csi_print("csi_remove ok!\n");
 	return 0;
 }
